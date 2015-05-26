34cef3435957b685f0960db58b550606674a029a
Romain DOUMENC <romain@doumenc.eu>
2015-05-25 22:19:32 -0700

    Refactor code in debug dump to a cleaner interface

--- debug.go
+++ debug.go
@@ -6,11 +6,34 @@ package journald
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
+func (rec *Record) Dump(gviz io.Writer) {
+	fmt.Fprintf(gviz, "\"%p\" [label = %s];\n", rec, rec.Field)
+	fmt.Fprintf(gviz, "\"%p\" -> \"%p\";\n", rec, rec.next_field)
+	if rec.entryArray != nil {
+		fmt.Fprintf(gviz, "\"%p\" -> {\n", rec)
+		for _, entry := range rec.entryArray {
+			fmt.Fprintf(gviz, "\"%p\"\n", entry)
+		}
+		fmt.Fprintf(gviz, "} [color=\"blue\"];\n")
+	}
+	if rec.entry != nil {
+		fmt.Fprintf(gviz, "\"%p\" -> \"%p\" [color=\"blue\"];\n", rec, rec.entry)
+	}
+}
+
+func (entry *Entry) Dump(gviz io.Writer) {
+	fmt.Fprintf(gviz, "\"%p\" [label = \"%x\", shape=\"square\"];\n", entry, entry.Timestamp)
+	for _, rc := range entry.Records {
+		fmt.Fprintf(gviz, "\"%p\" -> \"%p\" [color=\"red\"];\n", entry, rc)
+	}
+}
+
 // Dump a log structure to a graphviz representation
-func Dump_to_graphviz(log Log, filename string) {
+func (log Log) Dump(filename string) {
 	gviz, err := os.Create(filename)
 	defer gviz.Close()
 	if err != nil {
@@ -22,18 +45,22 @@ func Dump_to_graphviz(log Log, filename string) {
 
 	// Simply go over existing records
 	for _, rec := range log.recmap {
-		fmt.Fprintf(gviz, "%p -> %p [label = %s];\n", rec, rec.next_field, rec.Field)
-		if rec.entryArray != nil {
-			for _, entry := range rec.entryArray {
-				fmt.Fprintf(gviz, "%p -> %p;\n", rec, entry)
-			}
-		}
-		if rec.entry != nil {
-			fmt.Fprintf(gviz, "%p -> %p;\n", rec, rec.entry)
-		}
+		rec.Dump(gviz)
 	}
 
 	// Also go over entries for back pointer
+	for i := range log.currentEntry {
+		if log.currentEntry[i].Timestamp == 0 {
+			continue
+		}
+		log.currentEntry[i].Dump(gviz)
+	}
+
+	for _, bl := range log.backlogEntryArrays {
+		for i := range bl {
+			log.currentEntry[i].Dump(gviz)
+		}
+	}
 
 	// Finally, go over fields for back pointers
 	fmt.Fprintf(gviz, "}\n")
